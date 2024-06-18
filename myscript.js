4f61f58efd8715f1473abab6f03d319cb463939a
LorisGalleana <loris.galleana@gmail.com>
2024-06-18 17:13:35 +0200

    build: add img files

---
+++ myscript.js
@@ -0,0 +1,8 @@
+//creare array di oggetti per rappresentare i membri del team
+
+    //ogni membro(oggetto) è caratterizzato dalle seguenti informazioni: nome, ruolo e foto
+
+//stampare su console, per ogni membro del team, le informazioni di nome, ruole e la stringa della foto
+
+
+///stampare le stesse informazioni su DOM sottoforma di stringhe
