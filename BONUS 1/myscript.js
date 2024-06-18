eac3a682c78dbf46db031a31fe1eb8cd2321ce77
LorisGalleana <loris.galleana@gmail.com>
2024-06-18 19:16:09 +0200

    ESERCIZIO COMPLETO CON BONUS

--- BONUS 1/myscript.js
+++ BONUS 1/myscript.js
@@ -1,6 +1,5 @@
-const userName = document.querySelector(".member-name");
-const userRole = document.querySelector(".member-role");
-const userPhoto = document.querySelector(".member-photo");
+const container = document.querySelector(".container");
+
 
 
 //creare array di oggetti per rappresentare i membri del team
@@ -44,19 +43,31 @@ const team = [
 } */
 
 //stampare su console, per ogni membro del team, le informazioni di nome, ruole e la stringa della foto
-
-
-
-///stampare le stesse informazioni su DOM sottoforma di stringhe
-
-
 for (let i = 0; i < team.length; i++) {
     let teamMember = team[i]
     let memberName = teamMember.nome;
     let memberRole = teamMember.ruolo;
     let memberPhoto = teamMember.foto;
-    newDiv(userName,"Nome: ", memberName);
-    newDiv(userRole,"Ruolo: ", memberRole);
-    newImg(userPhoto, memberPhoto);
+    let createCard = createElementWithClass("div", "card");
+    appendElement(container, createCard)
+    const card = document.querySelectorAll(".card");
+    let createPhoto = createElementWithClass("div", "member-photo");
+    let createName = createElementWithClass("div", "member-name");
+    let createRole = createElementWithClass("div", "member-role");
+    appendElement(card[i], createPhoto)
+    appendElement(card[i], createName)
+    appendElement(card[i], createRole)
+    const userName = document.querySelectorAll(".member-name");
+    const userRole = document.querySelectorAll(".member-role");
+    const userPhoto = document.querySelectorAll(".member-photo");
+    newDiv(userName[i], memberName, "");
+    newDiv(userRole[i], memberRole, "");
+    newImg(userPhoto[i], memberPhoto);
 }
+
+
+///stampare le stesse informazioni su DOM sottoforma di stringhe
+
+
+
 
